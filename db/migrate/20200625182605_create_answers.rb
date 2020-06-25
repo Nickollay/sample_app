665520d3219fa569ba7c7941299bfdccc193c2d4
nickollay <nickollaysokolov@gmail.com>
2020-06-25 22:27:07 +0300

    change db to pg

--- db/migrate/20200625182605_create_answers.rb
+++ db/migrate/20200625182605_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.integer :correct, :default => 0, :null => false
-      t.reference :question, foreign_key: true
-      t.reference :user, foreign_key: true
+      t.references :question, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
