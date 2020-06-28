f6b4fdebd8acc139e7033c72b1a59a3b506e1cf9
nickollay <nickollaysokolov@gmail.com>
2020-06-28 10:40:23 +0300

    save point

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
     # id = current_user.id
     tests_ids = UserTest.where(user_id: id).pluck(:test_id)
     Test.where(id: tests_ids)
+
+
+    # Создайте инстанс-метод в модели User, который
+    # принимает
+    # в качестве аргумента значение уровня сложности и возвращает
+    # список всех Тестов, которые проходит
+    # или когда-либо проходил Пользователь на этом уровне сложности
+    Test.joins(:tests_users)
   end
 end
