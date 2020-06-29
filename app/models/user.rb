f23f4922a3a1e3a79a1b50027e1516cdb11e2d27
nickollay <nickollaysokolov@gmail.com>
2020-06-29 20:44:39 +0300

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     # id = current_user.id
   # TODO: after merging ex_5 change on that:
   #  Test.joins('JOIN tests_users ON tests.id = tests_users.test_id')
-  #         .where(tests: { level: 1}, tests_users: { user_id: 2 } )
+  #         .where(tests: { level: level}, tests_users: { user_id: id } )
     Test.joins('JOIN user_tests ON tests.id = user_tests.test_id')
         .where(tests: { level: level}, user_tests: { user_id: id } )
   end
