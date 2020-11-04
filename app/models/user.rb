255750efc85003f248caeb7de8b18bf89c7f54f2
nickollay <nickollaysokolov@gmail.com>
2020-11-04 15:39:32 +0300

    11-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,8 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
   end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
