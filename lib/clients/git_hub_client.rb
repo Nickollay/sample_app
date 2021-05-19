e03ad1d766be63ffa0f2e85d151b66148818f571
nickollay <nickollaysokolov@gmail.com>
2021-05-19 23:28:52 +0300

    Remove copmmented code

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,8 +1,4 @@
 class GitHubClient
-  # require 'faraday'
-  # # require 'faraday_middleware'
-  # require 'faraday_json'
-
   ROOT_ENDPOINT = 'https://api.github.com'
   ACCESS_TOKEN = ENV['GITHUB_TOKEN']
 
@@ -24,13 +20,6 @@ class GitHubClient
   private
 
   def http_client
-    # binding.pry
      Faraday.new(url: ROOT_ENDPOINT) #do |conn|
-     # Octokit::Client.new(access_token: ACCESS_TOKEN)
-    #   conn.request :json
-    #   conn.response :json
-    #
-    #   conn.adapter Faraday.default_adapter
-    # end
   end
 end
